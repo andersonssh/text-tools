434de2adce043c27ad9e46fcef9b3311b00f8f8e
andersonssh <4nd3r50n4r4uj0@gmail.com>
2022-01-11 18:31:23 -0300

    Add counters functions

---
+++ js/counters.js
@@ -0,0 +1,27 @@
+const countCharacters = (text) => {
+    return text.lenght
+}
+const countWords = (text) => {
+    return text.split(' ').lenght
+}
+const countOcurrences = (word, text) => {
+    let result = 0
+    let indexWord = 0
+    for(i of text){
+        if (word[indexWord] != i){ 
+            indexWord = 0
+        }
+        // if current letter is different then test the first letter of the word again
+        if (word[indexWord] == i){
+            if (indexWord == word.length - 1){
+                result++
+                indexWord = 0
+                continue
+            }else{
+                indexWord++
+            }
+        }  
+
+    }
+    return result
+}
