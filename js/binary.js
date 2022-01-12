6979fa9db8ba7a83873806b49b8d1039a16fc0e0
andersonssh <4nd3r50n4r4uj0@gmail.com>
2022-01-12 12:49:02 -0300

    Add binary functions

---
+++ js/binary.js
@@ -0,0 +1,25 @@
+const textToBinary = text => {
+    let result = []
+    let c = ''
+    for (i of text){
+        c = i.charCodeAt(0).toString(2)
+        c = c.length < 8 ? '0' + c: c
+        result.push(c)
+    }
+    return result.join(' ')
+}
+const binaryToText = binary => {
+    binary = binary.split(' ')
+    let ascii = []
+    let sum = 0
+    for(byte of binary){
+        byte = reverseText(byte)
+        sum = 0
+        for (bit in byte){
+            sum += byte[bit]*2**bit
+        }
+        ascii.push(sum)
+        
+    }
+    return ascii.join('')
+}
